8b3d4953d996a0bc2c655684756049bbb8f532f6
Linda <lindaqueentin701@gmail.com>
2026-03-12 15:57:45 +0530

    Remove the profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,6 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
-import MarketplaceScreen from './screens/MarketplaceScreen';
-import ForumScreen from './screens/ForumScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ProfileScreen from './screens/ProfileScreen';
 import JoinForum from './components/JoinForum';
 import { useLocation } from 'react-router-dom';
 
@@ -24,11 +20,8 @@ const App = () => {
             <main>
                 <Routes>
                     <Route path='/' element={<HomeScreen />} />
-                    <Route path='/marketplace' element={<Container className='py-3'><MarketplaceScreen /></Container>} />
-                    <Route path='/forum' element={<Container className='py-3'><ForumScreen /></Container>} />
                     <Route path='/join-forum' element={<JoinForum />} />
-                    <Route path='/register' element={<Container className='py-3'><RegisterScreen /></Container>} />
-                    <Route path='/profile' element={<Container className='py-3'><ProfileScreen /></Container>} />
+                  
                 </Routes>
             </main>
             <Footer />
